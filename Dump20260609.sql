b27ff48fd6287b2bd4da9901feccd152b7e0d232
dothang13 <endiezzhang13@gmail.com>
2026-06-10 00:37:51 +0700

    Temp

---
+++ Dump20260609.sql
@@ -0,0 +1,180 @@
+-- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+--
+-- Host: localhost    Database: pbl5
+-- ------------------------------------------------------
+-- Server version	8.0.44
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `audit_logs`
+--
+
+LOCK TABLES `audit_logs` WRITE;
+/*!40000 ALTER TABLE `audit_logs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `audit_logs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `certificates`
+--
+
+LOCK TABLES `certificates` WRITE;
+/*!40000 ALTER TABLE `certificates` DISABLE KEYS */;
+/*!40000 ALTER TABLE `certificates` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `chat_invitations`
+--
+
+LOCK TABLES `chat_invitations` WRITE;
+/*!40000 ALTER TABLE `chat_invitations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `chat_invitations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `conversation_members`
+--
+
+LOCK TABLES `conversation_members` WRITE;
+/*!40000 ALTER TABLE `conversation_members` DISABLE KEYS */;
+/*!40000 ALTER TABLE `conversation_members` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `conversations`
+--
+
+LOCK TABLES `conversations` WRITE;
+/*!40000 ALTER TABLE `conversations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `conversations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `event_approvals`
+--
+
+LOCK TABLES `event_approvals` WRITE;
+/*!40000 ALTER TABLE `event_approvals` DISABLE KEYS */;
+/*!40000 ALTER TABLE `event_approvals` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `event_documents`
+--
+
+LOCK TABLES `event_documents` WRITE;
+/*!40000 ALTER TABLE `event_documents` DISABLE KEYS */;
+/*!40000 ALTER TABLE `event_documents` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `event_feedbacks`
+--
+
+LOCK TABLES `event_feedbacks` WRITE;
+/*!40000 ALTER TABLE `event_feedbacks` DISABLE KEYS */;
+/*!40000 ALTER TABLE `event_feedbacks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `event_images`
+--
+
+LOCK TABLES `event_images` WRITE;
+/*!40000 ALTER TABLE `event_images` DISABLE KEYS */;
+/*!40000 ALTER TABLE `event_images` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `event_registrations`
+--
+
+LOCK TABLES `event_registrations` WRITE;
+/*!40000 ALTER TABLE `event_registrations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `event_registrations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `event_timeline_details`
+--
+
+LOCK TABLES `event_timeline_details` WRITE;
+/*!40000 ALTER TABLE `event_timeline_details` DISABLE KEYS */;
+/*!40000 ALTER TABLE `event_timeline_details` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `event_timelines`
+--
+
+LOCK TABLES `event_timelines` WRITE;
+/*!40000 ALTER TABLE `event_timelines` DISABLE KEYS */;
+/*!40000 ALTER TABLE `event_timelines` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `events`
+--
+
+LOCK TABLES `events` WRITE;
+/*!40000 ALTER TABLE `events` DISABLE KEYS */;
+/*!40000 ALTER TABLE `events` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `messages`
+--
+
+LOCK TABLES `messages` WRITE;
+/*!40000 ALTER TABLE `messages` DISABLE KEYS */;
+/*!40000 ALTER TABLE `messages` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `notification_recipients`
+--
+
+LOCK TABLES `notification_recipients` WRITE;
+/*!40000 ALTER TABLE `notification_recipients` DISABLE KEYS */;
+/*!40000 ALTER TABLE `notification_recipients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `notifications`
+--
+
+LOCK TABLES `notifications` WRITE;
+/*!40000 ALTER TABLE `notifications` DISABLE KEYS */;
+/*!40000 ALTER TABLE `notifications` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-06-09 21:54:51
